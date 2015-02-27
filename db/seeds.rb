3bc907b50309650200530df1f38a56c9a0a2b421
cpachomski <cpachomski@gmail.com>
2015-02-27 14:30:33 -0500

    add seeds and model associations for master

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+user_1 = User.create(email: 'user_1@gmail.com', username:'user_1', password: "123")
+user_2 = User.create(email: 'user_2@gmail.com', username:'user_2', password: "123")
+
+question_1 = Question.create(user_id: 1, title: "fun in the sun?", description: "how do I have fun anymore?", view_count: 0)
+question_2 = Question.create(user_id: 1, title: "pineapples?", description: "are they viable?", view_count: 1)
+
+answer_1 = Answer.create(description: 'yes they really are', user_id: 2, question_id: 2)
+answer_2 = Answer.create(description: "you can't you're old!", user_id: 2, question_id: 1)
+
+comment_1 = Comment.create(content: "ahhhhh noooo", user_id:2 , answer_id: 1)
+
+pineapples = Tag.create(name: 'pineapples')
+
+votes1 = Vote.create(vote_value: 1, user_id: 2)
+
+
