5cedd017445ecb95112419c27725a34fa862fc6d
rkartzman <remykartzman@gmail.com>
2015-02-27 17:38:23 -0500

    Start adding question vote logic

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,9 +1,11 @@
 class VotesController < ApplicationController
 
   def new
+    @vote = Vote.new
   end
 
   def create
+    @vote = Vote.create(votes_params)
   end
 
   def show
@@ -18,4 +20,10 @@ class VotesController < ApplicationController
   def destroy
   end
 
+
+  private 
+
+  def vote_params
+    params.require(:vote).permit(:vote_value, :user_id)
+  end 
 end
