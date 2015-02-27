fed5aceaea847bcd71a492a69f2ce9b7c5c1f75d
cpachomski <cpachomski@gmail.com>
2015-02-27 15:09:52 -0500

    create controllers with universal actions

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,33 @@
+class UserController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_or_create_by(user_params)
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
+
+end
