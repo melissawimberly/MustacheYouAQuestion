528f69faef1b6c97b6f297da223c7959c41f439d
garygz <garyginzburg@hotmail.com>
2015-02-27 15:53:30 -0500

    need to rebase to get latest fixes in master

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -1,4 +1,14 @@
 class QuestionController < ApplicationController
+  TAB_OPTION_LATEST = "latest".freeze
+  TAB_OPTION_WEEK = "week".freeze
+  TAB_OPTION_MONTH = "month".freeze
+  #show the list of questions based on the ?tab
+  #query. Default is latest
+  def index
+    tabOption = params[:tab]
+    @questions = nil
+
+  end
 
   def new
   end
