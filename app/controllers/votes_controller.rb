52bacd7d40237afe5b684a1ff30a973d1017ddc5
cpachomski <cpachomski@gmail.com>
2015-02-27 18:33:56 -0500

    add upvote and downvote, logic is flawed

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,6 +1,20 @@
 class VotesController < ApplicationController
 
   def new
+    vote = Vote.new(user_id: 1, vote_value: params[:vote_value])
+    answer = Answer.find( params[:answer_id])
+    p vote.vote_value
+    p params
+
+    p'*'*40
+
+    if vote.vote_value == 1
+      answer.votes.push(vote)
+    else
+      answer.votes.delete(answer.votes.count - 1)
+    end
+
+    redirect_to '/questions/2'
   end
 
   def create
@@ -16,6 +30,11 @@ class VotesController < ApplicationController
   end
 
   def destroy
+  end
+
+
+  def upvote
+    vote = Vote.new(user_id: 1, vote_value: params[:vote_value]
   end
 
 end
