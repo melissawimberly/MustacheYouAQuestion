292fcbdabdbd1f1d3c26b0daa65ac279b9e01abd
Melissa Wimberly <melissa.f.wimberly@gmail.com>
2015-02-28 14:49:32 -0500

    Add destroy, update and edit route logic

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -17,19 +17,27 @@ class QuestionsController < ApplicationController
 
 
   def edit
+    @question = Question.find_by(id: params[:id])
   end
 
   def update
+    question = Question.find_by(id: params[:id])
+    question.update_attributes(question_params)
+    redirect_to '/questions'
   end
 
   def destroy
+    @question = Question.find_by(id: params[:id])
+    @question.destroy
+    #bc we are not using ajax, we need to redirect after this is destroyed
+    redirect_to '/questions'
   end
 
   private
 
   def question_params
     params.require(:question).permit(:title, :description, :view_count, :user_id)
-  end 
+  end
 
 end
 
