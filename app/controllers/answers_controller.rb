9af0a30501b23b73ba29be46067f4a3cf1fa3438
cpachomski <cpachomski@gmail.com>
2015-02-28 13:38:38 -0500

    add show and create action methods to recieve new article information

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,9 +1,17 @@
 class AnswersController < ApplicationController
 
   def new
+    @answer = Answer.new
+    @question = Question.find(params[:id])
   end
 
   def create
+    @question = Question.find(params[:question_id])
+    @id = session[:user_id]
+    @answer = Answer.create(user_id: , description: params[:description], question_id: params[:question_id])
+    p'*'*50
+    p params[:question_id]
+    redirect_to "/questions/#{params[:question_id]}"
   end
 
   def show
@@ -18,5 +26,10 @@ class AnswersController < ApplicationController
 
   def destroy
   end
+
+   def answer_params
+    params.require(:answer).permit(:description, :user_id, :question_id)
+  end
+
 
 end
