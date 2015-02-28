d047a643f0b3d926307156c97f0c940ca31f4376
garygz <garyginzburg@hotmail.com>
2015-02-28 15:25:43 -0500

    Add creation of comments for an answer

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,22 @@
 class CommentsController < ApplicationController
 
   def new
+
+    @answer = Answer.find(params[:answer_id])
+    @comment = Comment.new(answer: @answer)
+
   end
 
   def create
-
+    question = Question.find(params[:question_id])
+    if make_sure_that_user_logged_in
+      #add comment
+      user = current_user
+      params[:comment][:user_id] = user.id
+      comment = Comment.create(question_params())
+      question.comments.push(comment)
+    end
+    redirect_to question_path(question)
   end
 
   def show
@@ -18,4 +30,29 @@ class CommentsController < ApplicationController
 
   def destroy
   end
+
+   private
+
+    def question_params
+      params.require(:comment).permit(:content, :user_id)
+    end
+
+  def make_sure_that_user_logged_in
+    user = current_user
+    if user == nil
+      @error = "Please login to add your comments"
+      return false
+    else
+      return true
+    end
+  end
+
+  def current_user
+    if session[:user_id]
+      User.find(session[:user_id])
+    else
+      return nil
+    end
+  end
+
 end
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -8,6 +8,9 @@ class TagsController < ApplicationController
   end
 
   def show
+    tag = Tag.find(params:id)
+    @questions = tag.questions
+    redirect_to questions_path
   end
 
   def edit
